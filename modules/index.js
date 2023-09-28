50ab332314962833922a9f0f620739679350a810
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-09-28 20:42:00 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/index.js
+++ modules/index.js
@@ -1,32 +1,84 @@
-import mods from "./*/index.js"
-import { getModules } from "./modules.js"
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, SafeAreaView } from 'react-native';
 
-export const modules = getModules(mods)
+const LoginScreen = () => {
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} style={styles.logo} />
+        <Text style={styles.logoText}>Login</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Password" secureTextEntry={true} style={styles.input} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Don't have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.footerLink}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>;
+};
 
-export const slices = modules
-  .filter(mod => mod.value.slice)
-  .map(mod => mod.value.slice)
-
-export const reducers = slices.reduce((acc, slice) => {
-  let name = slice.name.charAt(0).toUpperCase() + slice.name.slice(1)
-  acc[name] = slice.reducer
-  return acc
-}, {})
-
-export const navigators = modules
-  .filter(mod => mod.value.navigator)
-  .map(mod => {
-    return {
-      name: mod.name,
-      value: mod.value.navigator
-    }
-  })
-
-export const hooks = modules
-  .filter(mod => mod.value.hook)
-  .map(mod => {
-    return {
-      name: mod.name,
-      value: mod.value.hook
-    }
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 30
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 10
+  },
+  logoText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  inputContainer: {
+    marginBottom: 20
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: '#333',
+    borderRadius: 5,
+    paddingVertical: 10,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  footerText: {
+    marginRight: 5
+  },
+  footerLink: {
+    color: '#333',
+    fontWeight: 'bold'
+  }
+});
+export default LoginScreen;
